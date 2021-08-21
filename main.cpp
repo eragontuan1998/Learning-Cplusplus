10d156ec6890d0c6f68a56df1a92d9c52898df2f
Eragon Tuan <tuanitfsd@gmail.com>
2021-08-21 21:16:58 +0700

    enum in cplusplus

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
 using namespace std;
+enum week
+{
+    Monday = 2,
+    Tuesday = 3,
+    Wednesday = 4,
+    Thursday = 5,
+    Friday = 6,
+    Saturday = 7,
+    Sunday = 8
+};
 
 main()
 {
+    week mon = week::Monday;
+    cout << mon << endl;
 }
