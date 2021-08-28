a4ca453daeadfac0aed642646701c0fcdd45ba00
Eragon Tuan <tuanitfsd@gmail.com>
2021-08-28 15:50:50 +0700

    Hello World 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
+void swap(int x, int y);
 main()
 {
+    int a = 4, b = 10;
+    cout << "Before swap In main function: x = " << a << " y = " << b << endl; 
+    swap(a,b);
+    cout << "After swap In main function: x = " << a << " y = " << b << endl; 
+
+}
+void swap(int x, int y){
+    int temp = x;
+    x = y;
+    y = temp;
+    cout << "In swap function: x = " << x << " y = " << y << endl;
+    return;
 }
